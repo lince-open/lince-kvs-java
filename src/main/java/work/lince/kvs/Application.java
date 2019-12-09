fb09897a9482d5ce5584ad99f9cd83d64a5c33ae
Pedro Zatta <pedrozatta@gmail.com>
2019-12-09 14:24:32 -0300

    sonar conf

--- src/main/java/work/lince/kvs/Application.java
+++ src/main/java/work/lince/kvs/Application.java
@@ -1,11 +1,9 @@
-package work.lince;
+package work.lince.kvs;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "work.lince.commons")
 public class Application {
 
     public static void main(String[] args) {
