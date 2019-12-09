1cf7eba0d6426189cc505a4d5cd2cc68ec3eb2d9
Pedro Zatta <pedrozatta@gmail.com>
2019-12-09 13:13:49 -0300

    test

---
+++ src/test/java/work/lince/ApplicationTest.java
@@ -0,0 +1,13 @@
+package work.lince;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ApplicationTest {
+    
+    @Test
+    public void contextLoads() {
+        Assert.assertTrue(true);
+    }
+    
+}
