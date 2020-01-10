85c6b31b6dd7b667091e86c237b2edd3fde71e79
Pedro Zatta <pedrozatta@gmail.com>
2020-01-10 17:07:08 -0300

    add actuator

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ ARG JAR_FILE
 COPY ${JAR_FILE} /app.jar
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
-ENTRYPOINT ["/entrypoint.sh"]
+#ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["java", "-Xms64m", "-Xmx128m","-Dspring.profiles.active=prod", "-Djava.security.egd=file:/dev/./urandom", "-jar", "/app.jar"]
 EXPOSE 8080
--- src/main/java/work/lince/kvs/Application.java
+++ src/main/java/work/lince/kvs/Application.java
@@ -2,8 +2,11 @@ package work.lince.kvs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import work.lince.commons.rest.provider.filter.ResponseHeaderFilter;
 
 @SpringBootApplication(scanBasePackages = {"work.lince"})
+@ServletComponentScan(basePackageClasses = {ResponseHeaderFilter.class})
 public class Application {
 
     public static void main(String[] args) {
